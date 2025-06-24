d164402c62faaf14148ba944ace5c50c3d4c81c0
Aman Raj <amanraj6221@gmail.com>
2025-06-24 10:54:58 +0530

    updated

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="index.php"><i class="flaticon-university"></i>Scholarship <br><small>Management System</small></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+      </button>
+
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+          <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+          <li class="nav-item"><a href="schemes.php" class="nav-link">Schemes</a></li>
+          <li class="nav-item active"><a href="contact.php" class="nav-link">Contact</a></li>
+          <li class="nav-item active"><a href="admin/login.php" class="nav-link">Admin</a></li>
+          <li class="nav-item cta"><a href="users/login.php" class="nav-link"><span>Apply Now!</span></a></li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
